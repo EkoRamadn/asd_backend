f7df2e5800c64ba5061c7b97936079e7812e47fa
EkoRamadn <ekor4m4d4n1@gmail.com>
2025-06-16 22:42:24 +0700

    end point get data

---
+++ api/getprofile.js
@@ -0,0 +1,25 @@
+import { getOrCreatePemasukanHariIni } from "../utils/getOrCreatePemasukanHariIni.js";
+import { withAuth } from "../middleware/authMiddleware.js";
+import pool from "../lib/db.js";
+
+async function insertPenjualanDomba(req, res) {
+    if (req.method !== "GET") {
+        return res.status(405).json({ error: "Method Not Allowed" });
+    }
+
+    try {
+        const { id } = req.user
+        const data = await pool.query(
+            `SELECT username,email FROM account WHERE uid=$1`,
+            [id]
+        );
+        console.log(id)
+
+        res.status(201).json(data.rows);
+    } catch (error) {
+        console.error("Gagal insert penjualan domba:", error);
+        res.status(500).json({ error: "Terjadi kesalahan di server" });
+    }
+}
+
+export default withAuth(insertPenjualanDomba);
